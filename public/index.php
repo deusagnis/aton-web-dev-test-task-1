d367e7258f9b501b617ccd1f9045339c6e016d3e
Deusagnis <deusagnis@gmail.com>
2023-04-22 01:55:30 +0300

    Improve project structure.

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// provide environment
+// load config
+// connect to database
+// parse params
+// validate params
+// load products by params
+// render products page
+// or render error page
