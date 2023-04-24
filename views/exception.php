afd281702414b178e8e2399b712dbc363c9f789a
Deusagnis <deusagnis@gmail.com>
2023-04-24 16:38:55 +0300

    Apply template layout change.

--- views/exception.php
+++ views/exception.php
@@ -5,19 +5,17 @@ return function (Throwable $throwable, bool $debug = true) {
         $pre = print_r($throwable->toArray(false), true);
     }
     ?>
-    <div class="container">
-        <div class="row">
-            <div class="col text-center">
-                <h1>Exception <?= $throwable->getCode() ?></h1>
-                <h2><?= $throwable->getMessage() ?></h2>
-            </div>
+    <div class="row">
+        <div class="col text-center">
+            <h1>Exception <?= $throwable->getCode() ?></h1>
+            <h2><?= $throwable->getMessage() ?></h2>
         </div>
-        <div class="row">
-            <div class="col-auto mx-auto text-left">
-                <pre>
-                    <?= $pre ?>
-                </pre>
-            </div>
+    </div>
+    <div class="row">
+        <div class="col-auto mx-auto text-left">
+            <pre>
+                <?= $pre ?>
+            </pre>
         </div>
     </div>
     <?php
