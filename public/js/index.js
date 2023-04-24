9c1fcc0d4114c08b5a7e42a15582a9b9f7495f14
Deusagnis <deusagnis@gmail.com>
2023-04-24 16:41:01 +0300

    Describe page logic.

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,43 @@
-window.addEventListener("load", (event) => {
+import RenderProducts from "./RenderProducts.js";
+import ParseProductsResponse from "./ParseProductsResponse.js";
+import ProvideLocalSearch from "./ProvideLocalSearch.js";
+import ProductsSelection from "./ProductsSelection.js";
+import ProductsPageState from "./ProductsPageState.js";
+import ProvideGlobalSearch from "./ProvideGlobalSearch.js";
+import ProvidePagination from "./ProvidePagination.js";
+import ProvideLocalPriceSorting from "./ProvideLocalPriceSorting.js";
 
+window.addEventListener("load", () => {
+    const pageState = new ProductsPageState(
+        'serverOffsetInput',
+        'globalSearchInput',
+        'priceSortingSelect',
+        'countSelect'
+    )
+    pageState.parse(true)
+
+    const productsResponse = ParseProductsResponse.parse()
+
+    const productsRendering = new RenderProducts('productsTableBody')
+
+    const productsSelection = new ProductsSelection()
+
+    const localPriceSorting = new ProvideLocalPriceSorting('tablePriceSortBy', productsSelection)
+    localPriceSorting.provide()
+
+    productsSelection.addCallback(localPriceSorting.sortProducts)
+    productsSelection.addCallback(productsRendering.render)
+    productsSelection.setAllProducts(productsResponse['items'])
+
+    const localSearch = new ProvideLocalSearch(
+        'localSearchInput',
+        productsSelection
+    )
+    localSearch.provide()
+
+    const globalSearch = new ProvideGlobalSearch('globalSearchForm', pageState)
+    globalSearch.provide()
+
+    const pagination = new ProvidePagination('productsPagination', pageState, productsResponse['count'])
+    pagination.provide()
 });
